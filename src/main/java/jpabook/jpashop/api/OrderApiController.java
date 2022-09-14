0cb367dca348aae1ea86c86fc1c2f98c763a9f14
hyejin <hyejinbag772@gmail.com>
2022-09-15 08:50:02 +0900

    20220915 API 개발 고급 - 컬렉션 조회 최적화 - 주문 조회 V2: 엔티티를 DTO로 변환

--- src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -1,17 +1,22 @@
 package jpabook.jpashop.api;
 
+import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.OrderItem;
+import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
- * 일대다 관계 조회 및 최적화하는 방법
+ * 일대다(OneToMany) 관계 조회 및 최적화하는 방법
  */
 @RestController
 @RequiredArgsConstructor
@@ -21,6 +26,9 @@ public class OrderApiController {
 
     /**
      * 주문조회 v1: 엔티티 직접 노출
+     * 엔티티가 변하면 API 스펙이 변한다.
+     * 트랜잭션 안에서 지연로딩 필요
+     * 양방향 연관관계 문제가 있다 -> 무한루프 걸리지 않게 한쪽에 @JsonIgnore 해줘야함
      * 엔티티를 직접 노출하는 방법은 안좋습니다~~.. 비추 비추
      */
     @GetMapping("/api/v1/orders")
@@ -35,5 +43,60 @@ public class OrderApiController {
         }
 
         return all;
+    }
+
+    /**
+     * 주문 조회 v2: 엔티티를 DTO로 변환
+     * 지연로딩으로 인한 너무 많은 SQL이 실행됨 -> N + 1 문제 발생
+     * ORDER 1번
+     * MEMBER, ADDRESS N 번
+     * ORDERITEM N번
+     * ITEM N번 (ORDERITEM 조회 수 만큼)
+     * 1 + 2 + 2 + 2+ (2 + 2) = 11
+     */
+    @GetMapping("/api/v2/orders")
+    public List<OrderDto> ordersV2() {
+        List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+        List<OrderDto> result = orders.stream()
+                .map(o -> new OrderDto(o))
+                .collect(Collectors.toList());
+        return result;
+    }
+
+    @Data
+    static class OrderDto {
+
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+        //        private List<OrderItem> orderItems; // 엔티티를 이렇게 반환해서는 안된다. -> dto로 바꿔야한다.
+        private List<OrderItemDto> orderItems;
+        public OrderDto(Order order) {
+            orderId = order.getId();
+            name = order.getMember().getName();
+            orderDate = order.getOrderDate();
+            orderStatus = order.getStatus();
+            address = order.getDelivery().getAddress();
+//            order.getOrderItems().stream().forEach(o -> o.getItem().getName());
+//            orderItems = order.getOrderItems();
+            orderItems = order.getOrderItems().stream()
+                    .map(orderItem -> new OrderItemDto(orderItem))
+                    .collect(Collectors.toList());
+        }
+    }
+
+    @Data
+    static class OrderItemDto{
+
+        private String itemName; // 상품명
+        private int orderPrice; // 주문 가격
+        private int count; // 주문 수량
+        public OrderItemDto(OrderItem orderItem) {
+            itemName = orderItem.getItem().getName();
+            orderPrice = orderItem.getOrderPrice();
+            count = orderItem.getCount();
+        }
     }
 }
